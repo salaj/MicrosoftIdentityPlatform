c46a4415736d9f7ce920a9c61e24e638de820aee
Jakub Sala (JKSA) <jksa@demant.com>
2021-12-20 11:57:17 +0100

    authentication based on roles

---
+++ WebApp-OpenIDConnect-DelegateUserToCallApp2/Models/MessageModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp_OpenIDConnect_DelegateUserToCallApp2.Models
+{
+    public class MessageModel
+    {
+        [Required]
+        public string Message { get; set; }
+    }
+}
